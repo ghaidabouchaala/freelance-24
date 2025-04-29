e9006cea5f8300906dfe1c772807cbd79a48f6e8
ghaidabouchaala <git@stackblitz.com>
2025-04-29 23:49:57 +0100

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Freelance24 - Get Work Done in 24 Hours';
+export const SITE_DESCRIPTION = 'Connect with skilled freelancers who deliver quality work within 24 hours. Perfect for quick tasks and urgent projects.';
